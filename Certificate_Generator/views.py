7cb14544a67b6c3289ad080709b2f49c887ebc9d
AmanMishra0987 <aman0987mishra@gmail.com>
2024-07-12 00:01:09 +0530

    initial commit

---
+++ Certificate_Generator/views.py
@@ -0,0 +1,43 @@
+# This file is created by Aniket Raj Singh
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request,'index.html')
+def certificates(request):
+    certback1='https://raw.githubusercontent.com/AniketRajSingh/Certificate-Generator/aniket_master/static/admin/img/Certificate_template_1.png'
+    certback2='https://raw.githubusercontent.com/AniketRajSingh/Certificate-Generator/aniket_master/static/admin/img/Certificate_template_2.png'
+    certback3='https://raw.githubusercontent.com/AniketRajSingh/Certificate-Generator/aniket_master/static/admin/img/Certificate_template_3.png'
+    param={'name':'Aman','middlename':'Kumar','sirname':'Mishra','language':'Django','cert_background':certback1,'certback1':certback1,'certback2':certback2,'certback3':certback3,'gender':'his/her','gender1':'him/her','gender2':'Mr/Miss'}
+    firstn=request.POST.get('fname','Aman')
+    middlen=request.POST.get('mname','Kumar')
+    lastn=request.POST.get('lname','Mishra')
+    langn=request.POST.get('lang','Django')
+    gender=request.POST.get('gender','his/her')
+    gender1=gender
+    gender2='Mr/Miss'
+    certbackground=request.POST.get('certbackground','default')
+    if firstn == '' or langn=='' or certbackground=='default' or gender=='his/her':
+        defaultans='One or Multiple important blanks that includes First name, Language and background selection has been left blank, thus the system is generating default output with default selections. '
+        certbackground=certback1
+    else:
+        defaultans=''
+
+    if gender == 'male' :
+        gender='his'
+        gender1='him'
+        gender2='Mr'
+    elif gender =='female':
+        gender='her'
+        gender1='her'
+        gender2='Miss'
+
+    certbackground=request.POST.get('certbackground',certback1)
+    param.update({'name':firstn,'middlename':middlen,'sirname':lastn,'language':langn,'cert_background':certbackground,'defaultans':defaultans,'gender':gender,'gender1':gender1,'gender2':gender2})
+    return render(request,'certificates.html',param)
+def certform(request):
+    certback1='https://raw.githubusercontent.com/AniketRajSingh/Certificate-Generator/aniket_master/static/admin/img/Certificate_template_1.png'
+    certback2='https://raw.githubusercontent.com/AniketRajSingh/Certificate-Generator/aniket_master/static/admin/img/Certificate_template_2.png'
+    certback3='https://raw.githubusercontent.com/AniketRajSingh/Certificate-Generator/aniket_master/static/admin/img/Certificate_template_3.png'
+    param1={'certback1':certback1,'certback2':certback2,'certback3':certback3}
+    return render(request,'cert_gen_form.html',param1)
---
+++ README.md
@@ -0,0 +1,24 @@
+# Certificate-Generator
+ A Certificate Generating Website
+
+Initial Phase Takes Input From Users and Generate a Certificate
+
+ Future Goals - <!--Give Multiple Certificate Templates for Certificate Generation.-->
+                    -- Update - Multi Background Support Added.
+                    -- Navigation Buttons Added
+             - Add Multiple Other Pages to this Project Such as About & Contact 
+             - Add Download Certificate Option 
+             - Create Multiple Certificates using a csv file.(looping through the Dictionary and using Dict.update() function.)
+             - Turning the Complete Project into a Single Page Application.
+             
+Any Suggestions-
+               mail: mohitrajingh223@gmail.com
+               linkdin: linkedin.com/in/aniket-singh-5ba125168
+               instagram: instagram.com/aniket.raj.singh
+               Ph Number: +91 7248432377
+               Whatsapp Link: https://api.whatsapp.com/send/?phone=7248432377&text&type=phone_number
+
+You Can Visit The App Here: https://certifyme-certificategenerator.herokuapp.com
+
+
+<img width="1470" alt="Screenshot 2022-10-23 at 7 53 19 PM" src="https://user-images.githubusercontent.com/65419450/197397446-c5a28990-4451-4161-ae77-109b8d06924f.png">
